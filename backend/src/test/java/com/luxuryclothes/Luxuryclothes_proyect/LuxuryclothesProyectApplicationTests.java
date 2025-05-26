7b9f407ba8026362a482a84028d5024553b43498
FrancoCabelloDev <franco_alex_07@hotmail.com>
2025-05-26 14:05:13 -0500

    Se subió todo lo relacionado al login Auth y también se creo el apartado home con solo texto

---
+++ backend/src/test/java/com/luxuryclothes/Luxuryclothes_proyect/LuxuryclothesProyectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luxuryclothes.Luxuryclothes_proyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxuryclothesProyectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
